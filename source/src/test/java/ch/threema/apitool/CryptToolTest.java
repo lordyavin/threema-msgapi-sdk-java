38f5e7dd3a88f7f32e1b031120477f6ee293ff53
Kai Klesatschke <github@klesatschke.net>
2022-10-26 22:19:14 +0200

    Increases compiler level to Java 17, uses JUnit5 and Assertj

--- source/src/test/java/ch/threema/apitool/CryptToolTest.java
+++ source/src/test/java/ch/threema/apitool/CryptToolTest.java
@@ -24,75 +24,76 @@
 
 package ch.threema.apitool;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 import com.neilalexander.jnacl.NaCl;
 
 import ch.threema.apitool.messages.TextMessage;
-import ch.threema.apitool.messages.ThreemaMessage;
-import ch.threema.apitool.results.EncryptResult;
 
-public class CryptToolTest {
+class CryptToolTest {
 
-    @Test
-    public void testRandomNonce() throws Exception {
-        byte[] randomNonce = CryptTool.randomNonce();
+  @Test
+  void testRandomNonce() throws Exception {
+    var randomNonce = CryptTool.randomNonce();
 
-        // random nonce should be a byte array
-        Assert.assertNotNull("random nonce", randomNonce);
+    // with a length of 24
+    assertThat(randomNonce).hasSize(NaCl.NONCEBYTES);
+  }
 
-        // with a length of 24
-        Assert.assertSame("nonce length", randomNonce.length, NaCl.NONCEBYTES);
-    }
+  @Test
+  void testCreateKeyPair() {
+    var privateKey = new byte[NaCl.SECRETKEYBYTES];
+    var publicKey = new byte[NaCl.PUBLICKEYBYTES];
 
-    @Test
-    public void testCreateKeyPair() {
-        byte[] privateKey = new byte[NaCl.SECRETKEYBYTES];
-        byte[] publicKey = new byte[NaCl.PUBLICKEYBYTES];
+    CryptTool.generateKeyPair(privateKey, publicKey);
 
-        CryptTool.generateKeyPair(privateKey, publicKey);
+    assertThat(privateKey).isNotNull().satisfies(Common::hasContent);
+    assertThat(publicKey).isNotNull().satisfies(Common::hasContent);
+  }
 
-        Assert.assertFalse("empty private key", Common.isEmpty(privateKey));
-        Assert.assertFalse("empty public key", Common.isEmpty(publicKey));
-    }
+  @Test
+  void testDecrypt() throws Exception {
+    var nonce = "0a1ec5b67b4d61a1ef91f55e8ce0471fee96ea5d8596dfd0";
+    var box =
+        "45181c7aed95a1c100b1b559116c61b43ce15d04014a805288b7d14bf3a993393264fe554794ce7d6007233e8ef5a0f1ccdd704f34e7c7b77c72c239182caf1d061d6fff6ffbbfe8d3b8f3475c2fe352e563aa60290c666b2e627761e32155e62f048b52ef2f39c13ac229f393c67811749467396ecd09f42d32a4eb419117d0451056ac18fac957c52b0cca67568e2d97e5a3fd829a77f914a1ad403c5909fd510a313033422ea5db71eaf43d483238612a54cb1ecfe55259b1de5579e67c6505df7d674d34a737edf721ea69d15b567bc2195ec67e172f3cb8d6842ca88c29138cc33e9351dbc1e4973a82e1cf428c1c763bb8f3eb57770f914a";
 
-    @Test
-    public void testDecrypt() throws Exception {
-        String nonce = "0a1ec5b67b4d61a1ef91f55e8ce0471fee96ea5d8596dfd0";
-        String box = "45181c7aed95a1c100b1b559116c61b43ce15d04014a805288b7d14bf3a993393264fe554794ce7d6007233e8ef5a0f1ccdd704f34e7c7b77c72c239182caf1d061d6fff6ffbbfe8d3b8f3475c2fe352e563aa60290c666b2e627761e32155e62f048b52ef2f39c13ac229f393c67811749467396ecd09f42d32a4eb419117d0451056ac18fac957c52b0cca67568e2d97e5a3fd829a77f914a1ad403c5909fd510a313033422ea5db71eaf43d483238612a54cb1ecfe55259b1de5579e67c6505df7d674d34a737edf721ea69d15b567bc2195ec67e172f3cb8d6842ca88c29138cc33e9351dbc1e4973a82e1cf428c1c763bb8f3eb57770f914a";
+    var privateKey = Key.decodeKey(Common.otherPrivateKey);
+    var publicKey = Key.decodeKey(Common.myPublicKey);
 
-        Key privateKey = Key.decodeKey(Common.otherPrivateKey);
-        Key publicKey = Key.decodeKey(Common.myPublicKey);
+    var message =
+        CryptTool.decryptMessage(
+            DataUtils.hexStringToByteArray(box),
+            privateKey.key,
+            publicKey.key,
+            DataUtils.hexStringToByteArray(nonce));
 
-        ThreemaMessage message = CryptTool.decryptMessage(DataUtils.hexStringToByteArray(box), privateKey.key,
-                publicKey.key, DataUtils.hexStringToByteArray(nonce));
+    assertThat(message)
+        .isInstanceOf(TextMessage.class)
+        .extracting(msg -> ((TextMessage) msg).getText())
+        .isEqualTo("Dies ist eine Testnachricht. äöü");
+  }
 
-        Assert.assertNotNull(message);
-        Assert.assertTrue("message is not a instance of text message", message instanceof TextMessage);
-        Assert.assertEquals(((TextMessage) message).getText(), "Dies ist eine Testnachricht. äöü");
-    }
+  @Test
+  void testEncrypt() throws Exception {
+    var text = "Dies ist eine Testnachricht. äöü";
 
-    @Test
-    public void testEncrypt() throws Exception {
-        String text = "Dies ist eine Testnachricht. äöü";
+    var privateKey = Key.decodeKey(Common.myPrivateKey);
+    var publicKey = Key.decodeKey(Common.otherPublicKey);
 
-        Key privateKey = Key.decodeKey(Common.myPrivateKey);
-        Key publicKey = Key.decodeKey(Common.otherPublicKey);
+    var res = CryptTool.encryptTextMessage(text, privateKey.key, publicKey.key);
 
-        EncryptResult res = CryptTool.encryptTextMessage(text, privateKey.key, publicKey.key);
-        Assert.assertNotNull(res);
-        Assert.assertNotNull(res.getNonce());
-        Assert.assertNotNull(res.getResult());
-        Assert.assertFalse(Common.isEmpty(res.getNonce()));
-        Assert.assertFalse(Common.isEmpty(res.getResult()));
-    }
+    assertThat(res).isNotNull();
+    assertThat(res.getNonce()).isNotNull().satisfies(Common::hasContent);
+    assertThat(res.getResult()).isNotNull().satisfies(Common::hasContent);
+  }
 
-    @Test
-    public void testDerivePublicKey() throws Exception {
-        Key privateKey = Key.decodeKey(Common.myPrivateKey);
-        Key publicKey = Key.decodeKey(Common.myPublicKey);
-        byte[] derivedPublicKey = CryptTool.derivePublicKey(privateKey.key);
-        Assert.assertNotNull("derived public key", derivedPublicKey);
-        Assert.assertEquals(derivedPublicKey, publicKey.key);
-    }
+  @Test
+  void testDerivePublicKey() throws Exception {
+    var privateKey = Key.decodeKey(Common.myPrivateKey);
+    var publicKey = Key.decodeKey(Common.myPublicKey);
+    var derivedPublicKey = CryptTool.derivePublicKey(privateKey.key);
+    assertThat(derivedPublicKey).isEqualTo(publicKey.key);
+  }
 }
--- source/src/test/java/ch/threema/apitool/KeyTest.java
+++ source/src/test/java/ch/threema/apitool/KeyTest.java
@@ -1,6 +1,4 @@
 /*
- * $Id$
- *
  * The MIT License (MIT)
  * Copyright (c) 2015 Threema GmbH
  *
@@ -24,48 +22,52 @@
 
 package ch.threema.apitool;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.Test;
+
 import ch.threema.apitool.exceptions.InvalidKeyException;
-import org.junit.Test;
 
-public class KeyTest {
+class KeyTest {
 
-	@Test
-	public void testDecodeWrongKey() {
-		try {
-			Key.decodeKey("imnotarealkey");
-		} catch (InvalidKeyException e) {
-			return;
-		}
-		Assert.assertFalse("could parse invalid key", true);
-	}
+  @Test
+  void testDecodeWrongKey() {
+    assertThatExceptionOfType(InvalidKeyException.class)
+        .isThrownBy(() -> Key.decodeKey("imnotarealkey"));
+  }
 
-	@Test
-	public void testDecodeKeyPrivate() throws Exception {
-		Key key = Key.decodeKey("private:1234567890123456789012345678901234567890123456789012345678901234");
-		Assert.assertNotNull("key instance", key);
+  @Test
+  void testDecodeKeyPrivate() throws Exception {
+    var key =
+        Key.decodeKey("private:1234567890123456789012345678901234567890123456789012345678901234");
 
-		Assert.assertEquals(key.type, Key.KeyType.PRIVATE);
-		Assert.assertEquals(key.key, DataUtils.hexStringToByteArray("1234567890123456789012345678901234567890123456789012345678901234"));
-	}
-	@Test
-	public void testDecodeKeyPublic() throws Exception {
-		Key key = Key.decodeKey("public:1234567890123456789012345678901234567890123456789012345678901234");
-		Assert.assertNotNull("key instance", key);
+    assertThat(key).isNotNull();
+    assertThat(key.type).isEqualTo(Key.KeyType.PRIVATE);
+    assertThat(key.key)
+        .isEqualTo(
+            DataUtils.hexStringToByteArray(
+                "1234567890123456789012345678901234567890123456789012345678901234"));
+  }
 
-		Assert.assertEquals(key.type, Key.KeyType.PUBLIC);
-		Assert.assertEquals(key.key, DataUtils.hexStringToByteArray("1234567890123456789012345678901234567890123456789012345678901234"));
-	}
+  @Test
+  void testDecodeKeyPublic() throws Exception {
+    var key =
+        Key.decodeKey("public:1234567890123456789012345678901234567890123456789012345678901234");
+    assertThat(key).isNotNull();
+    assertThat(key.type).isEqualTo(Key.KeyType.PUBLIC);
+    assertThat(key.key)
+        .isEqualTo(
+            DataUtils.hexStringToByteArray(
+                "1234567890123456789012345678901234567890123456789012345678901234"));
+  }
 
-	@Test
-	public void testEncodePrivate() throws Exception {
-		byte[] keyAsByte = DataUtils.hexStringToByteArray(Common.myPrivateKeyExtract);
-
-		Key key = new Key(Key.KeyType.PRIVATE, keyAsByte);
-		Assert.assertNotNull("key instance", key);
-
-		Assert.assertEquals(Key.KeyType.PRIVATE, key.type);
-		Assert.assertEquals(key.key, keyAsByte);
-
-		Assert.assertEquals(key.encode(), Common.myPrivateKey);
-	}
+  @Test
+  void testEncodePrivate() throws Exception {
+    var keyAsByte = DataUtils.hexStringToByteArray(Common.myPrivateKeyExtract);
+    var key = new Key(Key.KeyType.PRIVATE, keyAsByte);
+    assertThat(key.type).isEqualTo(Key.KeyType.PRIVATE);
+    assertThat(key.key).isEqualTo(keyAsByte);
+    assertThat(key.encode()).isEqualTo(Common.myPrivateKey);
+  }
 }
